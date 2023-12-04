60c2b717ca69ccb7e4cf44e4606fe373401176eb
IA <itachi522ru@gmail.com>
2023-12-05 00:58:59 +0300

    Update downloader.sh

--- google/downloader.sh
+++ google/downloader.sh
@@ -15,6 +15,11 @@ get_prefix 'google' || echo 'failed'
 
 # save ipv4
 grep -v ':' /tmp/google.txt | sed 's/\/32//g' > /tmp/google-ipv4.txt
+sed -i '/^0.0.0.0/d' /tmp/google-ipv4.txt
+sed -i '/^127.0.0.1/d' /tmp/google-ipv4.txt
+# Exclude Google DNS subnet's
+sed -i '/^8.8.4.0/d' /tmp/google-ipv4.txt
+sed -i '/^8.8.8.0/d' /tmp/google-ipv4.txt
 
 # save ipv6
 #grep ':' /tmp/google.txt > /tmp/google-ipv6.txt
