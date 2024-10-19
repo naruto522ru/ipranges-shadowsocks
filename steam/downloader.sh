fa0ad22dfebaf172284209188c76b4210f71cfc1
IA (naruto522ru) <itachi522ru@gmail.com>
2024-10-19 11:04:10 +0300

    Replace valve on steam

--- steam/downloader.sh
+++ steam/downloader.sh
@@ -11,20 +11,20 @@ curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubuserconte
 curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/naruto522ru/ipranges/main/"$1"/ipv6_merged.txt >> /tmp/"$1".txt
 }
 
-get_prefix 'valve' || echo 'failed'
+get_prefix 'steam' || echo 'failed'
 
 # save ipv4
-grep -v ':' /tmp/valve.txt | sed 's/\/32//g' > /tmp/valve-ipv4.txt
+grep -v ':' /tmp/steam.txt | sed 's/\/32//g' > /tmp/steam-ipv4.txt
 
 # save ipv6
-#grep ':' /tmp/valve.txt > /tmp/valve-ipv6.txt
+#grep ':' /tmp/steam.txt > /tmp/steam-ipv6.txt
 
 # Create/Prepare ACL List for Shadowsocks IPv4
-echo -e "[bypass_all]\n[proxy_list]" | tee valve/ipv4.acl
+echo -e "[bypass_all]\n[proxy_list]" | tee steam/ipv4.acl
 
 # Create/Prepare ACL List for Shadowsocks IPv6
-#echo -e "[bypass_all]\n[proxy_list]" | tee valve/ipv6.acl
+#echo -e "[bypass_all]\n[proxy_list]" | tee steam/ipv6.acl
 
 # sort & uniq
-sort -h /tmp/valve-ipv4.txt | uniq >> valve/ipv4.acl
-#sort -h /tmp/valve-ipv6.txt | uniq >> valve/ipv6.acl
+sort -h /tmp/steam-ipv4.txt | uniq >> steam/ipv4.acl
+#sort -h /tmp/steam-ipv6.txt | uniq >> steam/ipv6.acl
